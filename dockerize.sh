438ae185981cbfd03ecdd75d422014e45cce8151
Ömer Faruk Almali <omerfaruk.almali@gmail.com>
2016-08-01 14:03:43 +0300

    #22 @ayseyo add dockerize script

---
+++ dockerize.sh
@@ -0,0 +1,13 @@
+DIR=`dirname $(readlink -f $0)`
+OLDPWD=`pwd`
+
+cd $DIR/../
+
+go clean
+go build
+
+docker login -u $DOCKER_USERNAME -e $DOCKER_EMAIL -p $DOCKER_PASSWORD
+docker build -t thoughtworksturkey/marketwatcher-data-ingestion-service .
+docker push thoughtworksturkey/marketwatcher-data-ingestion-service
+
+cd $OLDPWD
